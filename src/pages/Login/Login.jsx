84a78dae164118b896faec3f80cb3535029fc1ce
Romar476 <134797255+Romar476@users.noreply.github.com>
2024-07-18 20:00:32 +0200

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -47,14 +47,8 @@ export const Login = () => {
     return (
         <div className="Login">
             <form className="Login-form" ref={ formulario } onSubmit={ postLogin }>
-                <fieldset className="Login-fieldset">
-                    <label className="Login-label">Usuario</label>
-                    <input className="Login-input" type="text" name="user" placeholder="Usuario" required />
-                </fieldset>
-                <fieldset className="Login-fieldset">
-                    <label className="Login-label">Contraseña</label>
-                    <input className="Login-input" type="password" name="pass" placeholder="Contraseña" required />
-                </fieldset>
+                <input className="Login-input" type="text" name="user" placeholder="Usuario" required />
+                <input className="Login-input" type="password" name="pass" placeholder="Contraseña" required />
                 <button className="Login-btn">Iniciar sesión</button>
             </form>
 
